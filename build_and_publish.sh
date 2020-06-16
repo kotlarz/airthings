80579812ef657ec2248d0f9d1109a7b756928b4a
kotlarz <marius.kotlarz@gmail.com>
2020-06-16 16:15:45 +0200

    Release version 2.1.1

--- build_and_publish.sh
+++ build_and_publish.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 rm -rf ./airthings.egg-info/ ./dist/ ./build/
 python setup.py sdist bdist_wheel
-twine upload --repository testpypi dist/*
+#twine upload --repository testpypi dist/*
+twine upload dist/*
 
