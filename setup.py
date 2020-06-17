993bced81b7d808d346b204708c49894b1762077
kotlarz <marius.kotlarz@gmail.com>
2020-06-17 17:39:17 +0200

    Release version 2.1.3

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ except ImportError:
 
 setuptools.setup(
     name="airthings",
-    version="2.1.2",
+    version="2.1.3",
     author="Marius Kotlarz",
     author_email="marius@kotlarz.no",
     description="Fetch sensor measurements from Airthings devices",
